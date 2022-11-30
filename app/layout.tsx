cb4c83becc6ba5f5824abce91f9b6e71177e8f46
aero <zerowangue@163.com>
2022-11-30 23:54:49 +0800

    feat: pages directory

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,21 @@
-import './globals.css'
+import "./globals.css";
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="zh">
       {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+        <header>top-header</header>
+        {children}
+      </body>
     </html>
-  )
+  );
 }
